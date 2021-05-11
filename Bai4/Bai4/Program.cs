6335bd7eb82b3281bc501ce93f7f2321f313cc3c
Minh Tu <minhtu99.hubt@gmail.com>
2021-05-11 15:02:08 +0700

    done

---
+++ Bai4/Bai4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gia tri trung binh la: ");
+            int a, b, c, d;
+            int ketqua;
+            Console.WriteLine(" nhap gia tri a:");
+            Console.WriteLine(" nhap gia tri b:");
+            Console.WriteLine(" nhap gia tri c:");
+            Console.WriteLine(" nhap gia tri d:");
+
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = Convert.ToInt32(Console.ReadLine());
+
+            ketqua = (a + b + c + d) / 4;
+
+            Console.WriteLine(((a + "+" + b + "+" + c + "+" + d)+ "/" +4) + "=" + ketqua );
+            Console.ReadKey();
+
+
+        }
+    }
+}
